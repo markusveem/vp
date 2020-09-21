e2455cc14af97139efb3e04ca66e0f027d6549fe
markus veem <markusveem@users.noreply.github.com>
2020-09-22 00:11:04 +0300

    kolmas kodutoo

---
+++ tund3/motebaas.php
@@ -0,0 +1,26 @@
+<?php
+ require("../../../config.php");
+ $database = "if20_markus_ve_1";
+ //loen lehele kõik olemas olevad mõtted
+   $conn = new mysqli ($serverhost, $serverusername, $serverpassword, $database );
+   $stmt = $conn->prepare("SELECT idea FROM myidaes");
+   echo $conn->error;
+   //seome tulemuse muutuja
+   $stmt->bind_result($ideafromdb);
+   $stmt->execute();
+   $ideahtml="";
+   while($stmt->fetch()) {
+    $ideahtml .= "<p>" . $ideafromdb . "</p>";
+	
+   }  
+   $stmt->close();
+   $conn->close();
+	
+	
+require("header.php");
+?>
+
+
+  <?php echo $ideahtml; ?>
+   <p>tagasi koju. <a href="home2.php">ehk avalehele</a>
+   <p> <a href="sisaldamote.php">tahad veel motteid sisestada</a>
